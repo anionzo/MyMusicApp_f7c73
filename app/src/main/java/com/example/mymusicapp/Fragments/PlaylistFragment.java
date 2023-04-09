36dabe84f3f26264fd40fa3ea0c95e009653b3d7
mimieu-admin <tienyoyoyo@gmail.com>
2023-04-09 13:04:40 +0700

    playlist

---
+++ app/src/main/java/com/example/mymusicapp/Fragments/PlaylistFragment.java
@@ -0,0 +1,27 @@
+package com.example.mymusicapp.Fragments;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.mymusicapp.R;
+
+
+public class PlaylistFragment extends Fragment {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_playlist, container, false);
+        return view;
+    }
+}
