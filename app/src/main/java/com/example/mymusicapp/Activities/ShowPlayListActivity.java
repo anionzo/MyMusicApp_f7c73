f5ffee893a873ecc49bc6a7686cacd6911952ee7
mimieu-admin <tienyoyoyo@gmail.com>
2023-05-06 09:57:30 +0700

    update

---
+++ app/src/main/java/com/example/mymusicapp/Activities/ShowPlayListActivity.java
@@ -0,0 +1,16 @@
+package com.example.mymusicapp.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.mymusicapp.R;
+
+public class ShowPlayListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_show_play_list);
+    }
+}
